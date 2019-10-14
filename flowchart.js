4821db967798b743bcea25a5de7244c71ffeeff0
Elias Braut <eliasbraut@hotmail.com>
2019-10-14 12:01:43 +0200

    k

--- flowchart.js
+++ flowchart.js
@@ -2,11 +2,17 @@
 
 function setup() {
 
+let divMain = document.getElementById("main")
 let btnBox = document.getElementById("box")
 let btnCircle = document.getElementById("circle")
 let btnArrow = document.getElementById("arrow")
 
 btnBox.addEventListener("click", create)
+
+let box = [];
+
+divMain.appendChild(btnBox);
+box.push(create);
 
 function create() {
     let btnBox = document.createElement("div");
@@ -14,8 +20,6 @@ function create() {
     btnBox.style.height = "100px";
     btnBox.style.background = "red";
     btnBox.style.color = "white";
-
-document.getElementById("main").appendChild(btnBox);
 }
 
 }
