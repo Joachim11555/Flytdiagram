6bcf2050e6331407fac2f136a30df29eec867eb7
Elias Braut <eliasbraut@hotmail.com>
2019-10-14 11:45:21 +0200

    k

--- flowchart.js
+++ flowchart.js
@@ -7,5 +7,11 @@ let btnArrow = document.getElementById("arrow")
 btnBox.addEventListener("click", create)
 
 function create() {
-    let 
+    let div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "grey";
+    div.style.color = "white";
+
+document.getElementById("main").appendChild(div);
 }
